6f6aeb0970920fc4524cc94c3caca3e8282d657c
luis486 <luis.murcia1@u.icesi.edu.co>
2024-03-29 19:55:08 -0500

    Done
    eccomerce strore up

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("e495c094-584c-4daf-9ca4-a43dbaf1d68a");
 
   return (
     <Container>
